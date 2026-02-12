355ea5c43ddbae8fa9ed3d59b4879943d5dd900a
orafaelpessoa <rafael.pessoa.alexandre@gmail.com>
2026-02-12 16:50:57 -0300

    middleware atualizado

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,45 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const isLoggedIn = request.cookies.get('sb-access-token')
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next()
 
-  if (!isLoggedIn && request.nextUrl.pathname.startsWith('/dashboard')) {
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: any) {
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: any) {
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+ const {
+  data: { user },
+} = await supabase.auth.getUser()
+
+if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
+
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  return NextResponse.next()
+  return response
 }
 
 export const config = {
